a1ded614fab4cd98d71c8adbb18d2fcdabac8e22
Leonel Suarez <153206826+SuarezLeonel@users.noreply.github.com>
2024-06-05 12:27:05 -0300

    Create Punto2.php

---
+++ TP1/Punto2.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title>Captura de datos del form</title>
+</head>
+<body>
+<?php
+echo "El nombre ingresado es: ";
+echo $_REQUEST['nombre'] . "<br>";
+if ($_REQUEST['estudiosP']){
+echo "Tiene estudios primarios.\n";
+} else if ($_REQUEST['estudioss']){
+echo "Tiene estudios secundarios.\n";
+} else if ($_REQUEST['estudiosT']){
+echo "Tiene estudios teceriarios.\n";
+} else if ($_REQUEST['estudiosU']){
+echo "Tiene estudios universitarios.\n";
+} else if ($_REQUEST['estudiosSE']){
+echo "No tiene estudios.\n";
+}
+?>
+</body>
+</html> 
