1df91a8f2dd2ac6fffc1821df2586c5633bb4160
Leonel Suarez <153206826+SuarezLeonel@users.noreply.github.com>
2024-06-10 07:58:06 -0300

    Update Punto3.php

--- TP1/Punto3.php
+++ TP1/Punto3.php
@@ -1,1 +1,22 @@
-
+<html>
+<head>
+<title>Captura de datos del form</title>
+</head>
+<body>
+<?php
+echo "El nombre ingresado es: ";
+echo $_REQUEST['nombre'] . "<br>";
+$contador = 0;
+if (isset($_REQUEST['checkbox1'])){
+    $contador = $contador + 1;
+    } if (isset($_REQUEST['checkbox2'])){
+    $contador = $contador + 1;
+    } if (isset($_REQUEST['checkbox3'])){
+    $cotador = $contador + 1;
+    } if (isset($_REQUEST['checkbox4'])){
+        $cotador = $contador + 1;
+    } 
+    echo "Juega " . $contador . " deportes.";
+?>
+</body>
+</html>
