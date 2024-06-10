f17783e3407a6317ffa7e717a979779580597dd0
Leonel Suarez <153206826+SuarezLeonel@users.noreply.github.com>
2024-06-10 08:09:02 -0300

    Update Punto4.php

--- TP1/Punto4.php
+++ TP1/Punto4.php
@@ -1,1 +1,15 @@
-
+<html>
+<head>
+<title>Captura de datos del form</title>
+</head>
+<body>
+<?php
+echo "El nombre ingresado es: ";
+echo $_REQUEST['nombre'] . "<br>";
+$contador = 0;
+if ($_REQUEST['operacion'] == "tercero"){
+    echo "Debe pagar recargo.";
+} 
+?>
+</body>
+</html>
