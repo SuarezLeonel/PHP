c85cdf3e6afea39ffaea04812c17c43f7499cdf1
Leonel Suarez <153206826+SuarezLeonel@users.noreply.github.com>
2024-06-22 21:52:34 -0300

    Update Punto4.php

--- TP2/Punto4.php
+++ TP2/Punto4.php
@@ -1,6 +1,8 @@
 <html>
-<?php
-    $confirmado = array(true, false, true, false, true, false);
-    echo "El elemento con índice 0 es: " . $confirmado[0];
-?>
+<body>
+    <?php
+        $confirmado = array(true, false, true, false, true, false);
+        echo "El elemento con índice 0 es: " . ($confirmado[0] ? 'true' : 'false');
+    ?>
+</body>
 </html>
