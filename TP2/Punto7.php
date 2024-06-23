aeb5df59ceea498e18ddecd683365575cfdc733c
Leonel Suarez <153206826+SuarezLeonel@users.noreply.github.com>
2024-06-22 21:59:20 -0300

    Update Punto7.php

--- TP2/Punto7.php
+++ TP2/Punto7.php
@@ -1,1 +1,16 @@
-
+<html>
+<body>
+    <?php
+        $usuarios = array(
+            "usuario1" => "clave1",
+            "usuario2" => "clave2",
+            "usuario3" => "clave3",
+            "usuario4" => "clave4",
+            "usuario5" => "clave5"
+        );
+        foreach ($usuarios as $nombre => $clave) {
+            echo "Usuario: $nombre, Clave: $clave" . "<br>";
+        }
+    ?>
+</body>
+</html>
