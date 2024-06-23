3db71616265409c101743fece3398696822ffcab
Leonel Suarez <153206826+SuarezLeonel@users.noreply.github.com>
2024-06-22 21:49:47 -0300

    Update Punto6.php

--- TP2/Punto6.php
+++ TP2/Punto6.php
@@ -1,1 +1,11 @@
-
+<html>
+<body>
+    <?php
+        $meses = array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+        sort($meses);
+        for ($i = 0; $i < count($meses); $i++) {
+            echo $meses[$i] . "<br>";
+        }
+    ?>
+</body>
+</html>
