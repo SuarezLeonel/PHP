52b99612aff0c1ea42e3b18c7d6c7d3003717379
Leonel Suarez <153206826+SuarezLeonel@users.noreply.github.com>
2024-06-22 21:51:16 -0300

    Update Punto2.php

--- TP2/Punto2.php
+++ TP2/Punto2.php
@@ -1,6 +1,12 @@
+<!DOCTYPE html>
 <html>
-<?php
-  $coches = array("Cronos", "Hilux", "Amarok", "Ranger", "Onix", "Sandero", "Frontier", "Gol");
-  echo "El coche es: " . $coches[5];
-?>
+<head>
+    <title>Modelos de Coches</title>
+</head>
+<body>
+    <?php
+        $coches = array("Cronos", "Hilux", "Amarok", "Ranger", "Onix", "Sandero", "Frontier", "Gol");
+        echo "El coche es: " . $coches[5];
+    ?>
+</body>
 </html>
