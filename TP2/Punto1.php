35a9aca6e2868d6018f25da20d12a1baa9bfea15
Leonel Suarez <153206826+SuarezLeonel@users.noreply.github.com>
2024-06-22 21:32:45 -0300

    Update Punto1.php

--- TP2/Punto1.php
+++ TP2/Punto1.php
@@ -1,5 +1,7 @@
+<html>
 <?php
   $dias = array("Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo");
   echo "El primer dia de la semana es: " . $dias[0] . "<br>";
   echo "El ultimo dia de la semana es: " . $dias[6];
 ?>
+</html>
