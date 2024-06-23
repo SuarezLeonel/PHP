fca6729d64fcbc68f0ed6c12e172acf2f1471b35
Leonel Suarez <153206826+SuarezLeonel@users.noreply.github.com>
2024-06-22 21:53:23 -0300

    Update Punto5.php

--- TP2/Punto5.php
+++ TP2/Punto5.php
@@ -1,8 +1,8 @@
 <html>
 <body>
-<?php
-    $jugador = array("Messi", "Neymar", "Bravo", "Iniesta", "Alba");
-    echo "La alineación del equipo está compuesta por: " . $jugador[0] . ", " . $jugador[1] . ", " . $jugador[2] . ", " . $jugador[3] . " y " . $jugador[4] . ".";
-?>
+    <?php
+        $jugador = array("Messi", "Neymar", "Bravo", "Iniesta", "Alba");
+        echo "La alineación del equipo está compuesta por: " . $jugador[0] . ", " . $jugador[1] . ", " . $jugador[2] . ", " . $jugador[3] . " y " . $jugador[4] . ".";
+    ?>
 </body>
 </html>
