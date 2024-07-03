26e3eaaa7ed072ade88d090729c38bd2945ad2d4
Leonel Suarez <153206826+SuarezLeonel@users.noreply.github.com>
2024-07-03 11:46:33 -0300

    Create Punto4-index1.php

---
+++ TP3/Punto4-index1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pedido de Pizzas</title>
+</head>
+<body>
+    <form action="index2.php" method="post">
+        Nombre: <input type="text" name="nombre"><br>
+        Dirección: <input type="text" name="direccion"><br>
+        Teléfono: <input type="text" name="telefono"><br>
+        Pizza: <input type="text" name="pizza"><br>
+        Cantidad: <input type="number" name="cantidad"><br>
+        <input type="submit" value="Enviar Pedido">
+    </form>
+</body>
+</html>
