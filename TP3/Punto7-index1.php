04393e1a5265b5d873ca6468a8f6cf714d7b61f9
Leonel Suarez <153206826+SuarezLeonel@users.noreply.github.com>
2024-07-03 11:52:10 -0300

    Create Punto7-index1.php

---
+++ TP3/Punto7-index1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sumar Números</title>
+</head>
+<body>
+    <form action="index2.php" method="post">
+        Número 1: <input type="number" name="numero1"><br>
+        Número 2: <input type="number" name="numero2"><br>
+        <input type="submit" value="Sumar">
+    </form>
+</body>
+</html>
