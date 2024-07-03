9e489130e5105a6f6973e4505967034193e71783
Leonel Suarez <153206826+SuarezLeonel@users.noreply.github.com>
2024-07-03 11:48:43 -0300

    Update Punto4-index2.php

--- TP3/Punto4-index2.php
+++ TP3/Punto4-index2.php
@@ -1,1 +1,13 @@
+<?php
+    $nombre = $_POST['nombre'];
+    $direccion = $_POST['direccion'];
+    $telefono = $_POST['telefono'];
+    $pizza = $_POST['pizza'];
+    $cantidad = $_POST['cantidad'];
 
+    $ar = fopen("pedidos.txt", "a") or die("No se pudo abrir el archivo!");
+    fputs($ar, "Nombre: $nombre\nDirección: $direccion\nTeléfono: $telefono\nPizza: $pizza\nCantidad: $cantidad\n--------------------\n");
+    fclose($ar);
+
+    echo "Pedido guardado. <a href='pedido.php'>Hacer otro pedido</a>";
+?>
