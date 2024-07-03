c022b37e67a3632d896497192c8c0e90ece576fa
Leonel Suarez <153206826+SuarezLeonel@users.noreply.github.com>
2024-07-03 11:53:04 -0300

    Create Punto8-index1.php

---
+++ TP3/Punto8-index1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Par o Impar</title>
+</head>
+<body>
+    <form action="index2.php" method="post">
+        Número: <input type="number" name="numero"><br>
+        <input type="submit" value="Verificar">
+    </form>
+</body>
+</html>
