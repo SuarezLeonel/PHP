fac6284442b1e690c6dddb4b699a133012cada7d
Leonel Suarez <153206826+SuarezLeonel@users.noreply.github.com>
2024-07-03 11:50:22 -0300

    Create Punto6-index1.php

---
+++ TP3/Punto6-index1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro de Usuario</title>
+</head>
+<body>
+    <form action="procesar_registro.php" method="post">
+        Nombre de Usuario: <input type="text" name="usuario"><br>
+        Clave: <input type="password" name="clave1"><br>
+        Repetir Clave: <input type="password" name="clave2"><br>
+        <input type="submit" value="Registrar">
+    </form>
+</body>
+</html>
